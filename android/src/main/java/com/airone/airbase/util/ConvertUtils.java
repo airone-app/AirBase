8aaaa083e34f77e7275b6bb7cb47b091a84b1c9d
Troy <chenlongfeiceo@gmail.com>
2019-06-04 10:16:32 +0800

    [Android]AirBase

---
+++ android/src/main/java/com/airone/airbase/util/ConvertUtils.java
@@ -0,0 +1,244 @@
+package com.airone.airbase.util;
+
+import android.content.res.Resources;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by chenlongfei on 2019-05-29.
+ */
+public class ConvertUtils {
+    public static final int KB = 1024;
+
+    /**
+     * Input stream to output stream.
+     *
+     * @param is The input stream.
+     * @return output stream
+     */
+    public static ByteArrayOutputStream input2OutputStream(final InputStream is) {
+        if (is == null) return null;
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            byte[] b = new byte[KB];
+            int len;
+            while ((len = is.read(b, 0, KB)) != -1) {
+                os.write(b, 0, len);
+            }
+            return os;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Output stream to input stream.
+     *
+     * @param out The output stream.
+     * @return input stream
+     */
+    public ByteArrayInputStream output2InputStream(final OutputStream out) {
+        if (out == null) return null;
+        return new ByteArrayInputStream(((ByteArrayOutputStream) out).toByteArray());
+    }
+
+    /**
+     * Input stream to bytes.
+     *
+     * @param is The input stream.
+     * @return bytes
+     */
+    public static byte[] inputStream2Bytes(final InputStream is) {
+        if (is == null) return null;
+        return input2OutputStream(is).toByteArray();
+    }
+
+    /**
+     * Bytes to input stream.
+     *
+     * @param bytes The bytes.
+     * @return input stream
+     */
+    public static InputStream bytes2InputStream(final byte[] bytes) {
+        if (bytes == null || bytes.length <= 0) return null;
+        return new ByteArrayInputStream(bytes);
+    }
+
+    /**
+     * Output stream to bytes.
+     *
+     * @param out The output stream.
+     * @return bytes
+     */
+    public static byte[] outputStream2Bytes(final OutputStream out) {
+        if (out == null) return null;
+        return ((ByteArrayOutputStream) out).toByteArray();
+    }
+
+    /**
+     * Bytes to output stream.
+     *
+     * @param bytes The bytes.
+     * @return output stream
+     */
+    public static OutputStream bytes2OutputStream(final byte[] bytes) {
+        if (bytes == null || bytes.length <= 0) return null;
+        ByteArrayOutputStream os = null;
+        try {
+            os = new ByteArrayOutputStream();
+            os.write(bytes);
+            return os;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Input stream to string.
+     *
+     * @param is          The input stream.
+     * @param charsetName The name of charset.
+     * @return string
+     */
+    public static String inputStream2String(final InputStream is, final String charsetName) {
+        if (is == null || isSpace(charsetName)) return "";
+        try {
+            ByteArrayOutputStream baos = input2OutputStream(is);
+            if (baos == null) return "";
+            return baos.toString(charsetName);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * String to input stream.
+     *
+     * @param string      The string.
+     * @param charsetName The name of charset.
+     * @return input stream
+     */
+    public static InputStream string2InputStream(final String string, final String charsetName) {
+        if (string == null || isSpace(charsetName)) return null;
+        try {
+            return new ByteArrayInputStream(string.getBytes(charsetName));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Output stream to string.
+     *
+     * @param out         The output stream.
+     * @param charsetName The name of charset.
+     * @return string
+     */
+    public static String outputStream2String(final OutputStream out, final String charsetName) {
+        if (out == null || isSpace(charsetName)) return "";
+        try {
+            return new String(outputStream2Bytes(out), charsetName);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * String to output stream.
+     *
+     * @param string      The string.
+     * @param charsetName The name of charset.
+     * @return output stream
+     */
+    public static OutputStream string2OutputStream(final String string, final String charsetName) {
+        if (string == null || isSpace(charsetName)) return null;
+        try {
+            return bytes2OutputStream(string.getBytes(charsetName));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Value of dp to value of px.
+     *
+     * @param dpValue The value of dp.
+     * @return value of px
+     */
+    public static int dp2px(final float dpValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * Value of px to value of dp.
+     *
+     * @param pxValue The value of px.
+     * @return value of dp
+     */
+    public static int px2dp(final float pxValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * Value of sp to value of px.
+     *
+     * @param spValue The value of sp.
+     * @return value of px
+     */
+    public static int sp2px(final float spValue) {
+        final float fontScale = Resources.getSystem().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * Value of px to value of sp.
+     *
+     * @param pxValue The value of px.
+     * @return value of sp
+     */
+    public static int px2sp(final float pxValue) {
+        final float fontScale = Resources.getSystem().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // other utils methods
+    ///////////////////////////////////////////////////////////////////////////
+
+    private static boolean isSpace(final String s) {
+        if (s == null) return true;
+        for (int i = 0, len = s.length(); i < len; ++i) {
+            if (!Character.isWhitespace(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ android/src/main/java/com/airone/airbase/util/DeviceUtils.java
@@ -0,0 +1,219 @@
+package com.airone.airbase.util;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * Created by chenlongfei on 2019-05-29.
+ */
+public class DeviceUtils {
+    /**
+     * Return whether device is rooted.
+     *
+     * @return {@code true}: yes<br>{@code false}: no
+     */
+    public static boolean isDeviceRooted() {
+        String su = "su";
+        String[] locations = {"/system/bin/", "/system/xbin/", "/sbin/", "/system/sd/xbin/",
+                "/system/bin/failsafe/", "/data/local/xbin/", "/data/local/bin/", "/data/local/",
+                "/system/sbin/", "/usr/bin/", "/vendor/bin/"};
+        for (String location : locations) {
+            if (new File(location + su).exists()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Return whether ADB is enabled.
+     *
+     * @return {@code true}: yes<br>{@code false}: no
+     */
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public static boolean isAdbEnabled() {
+        return Settings.Secure.getInt(
+                Utils.getApp().getContentResolver(),
+                Settings.Global.ADB_ENABLED, 0
+        ) > 0;
+    }
+
+    /**
+     * Return the version name of device's system.
+     *
+     * @return the version name of device's system
+     */
+    public static String getSDKVersionName() {
+        return android.os.Build.VERSION.RELEASE;
+    }
+
+    /**
+     * Return version code of device's system.
+     *
+     * @return version code of device's system
+     */
+    public static int getSDKVersionCode() {
+        return android.os.Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * Return the android id of device.
+     *
+     * @return the android id of device
+     */
+    @SuppressLint("HardwareIds")
+    public static String getAndroidID() {
+        String id = Settings.Secure.getString(
+                Utils.getApp().getContentResolver(),
+                Settings.Secure.ANDROID_ID
+        );
+        return id == null ? "" : id;
+    }
+
+    private static boolean isAddressNotInExcepts(final String address, final String... excepts) {
+        if (excepts == null || excepts.length == 0) {
+            return !"02:00:00:00:00:00".equals(address);
+        }
+        for (String filter : excepts) {
+            if (address.equals(filter)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @SuppressLint({"MissingPermission", "HardwareIds"})
+    private static String getMacAddressByWifiInfo() {
+        try {
+            final WifiManager wifi = (WifiManager) Utils.getApp()
+                    .getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            if (wifi != null) {
+                final WifiInfo info = wifi.getConnectionInfo();
+                if (info != null) return info.getMacAddress();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    private static String getMacAddressByNetworkInterface() {
+        try {
+            Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
+            while (nis.hasMoreElements()) {
+                NetworkInterface ni = nis.nextElement();
+                if (ni == null || !ni.getName().equalsIgnoreCase("wlan0")) continue;
+                byte[] macBytes = ni.getHardwareAddress();
+                if (macBytes != null && macBytes.length > 0) {
+                    StringBuilder sb = new StringBuilder();
+                    for (byte b : macBytes) {
+                        sb.append(String.format("%02x:", b));
+                    }
+                    return sb.substring(0, sb.length() - 1);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    private static String getMacAddressByInetAddress() {
+        try {
+            InetAddress inetAddress = getInetAddress();
+            if (inetAddress != null) {
+                NetworkInterface ni = NetworkInterface.getByInetAddress(inetAddress);
+                if (ni != null) {
+                    byte[] macBytes = ni.getHardwareAddress();
+                    if (macBytes != null && macBytes.length > 0) {
+                        StringBuilder sb = new StringBuilder();
+                        for (byte b : macBytes) {
+                            sb.append(String.format("%02x:", b));
+                        }
+                        return sb.substring(0, sb.length() - 1);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    private static InetAddress getInetAddress() {
+        try {
+            Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
+            while (nis.hasMoreElements()) {
+                NetworkInterface ni = nis.nextElement();
+                // To prevent phone of xiaomi return "10.0.2.15"
+                if (!ni.isUp()) continue;
+                Enumeration<InetAddress> addresses = ni.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress inetAddress = addresses.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        String hostAddress = inetAddress.getHostAddress();
+                        if (hostAddress.indexOf(':') < 0) return inetAddress;
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Return the manufacturer of the product/hardware.
+     * <p>e.g. Xiaomi</p>
+     *
+     * @return the manufacturer of the product/hardware
+     */
+    public static String getManufacturer() {
+        return Build.MANUFACTURER;
+    }
+
+    /**
+     * Return the model of device.
+     * <p>e.g. MI2SC</p>
+     *
+     * @return the model of device
+     */
+    public static String getModel() {
+        String model = Build.MODEL;
+        if (model != null) {
+            model = model.trim().replaceAll("\\s*", "");
+        } else {
+            model = "";
+        }
+        return model;
+    }
+
+    /**
+     * Return an ordered list of ABIs supported by this device. The most preferred ABI is the first
+     * element in the list.
+     *
+     * @return an ordered list of ABIs supported by this device
+     */
+    public static String[] getABIs() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return Build.SUPPORTED_ABIS;
+        } else {
+            if (!TextUtils.isEmpty(Build.CPU_ABI2)) {
+                return new String[]{Build.CPU_ABI, Build.CPU_ABI2};
+            }
+            return new String[]{Build.CPU_ABI};
+        }
+    }
+}
